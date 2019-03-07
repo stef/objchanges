c1f68adf8b5184567fc069b876d6198cd24d4494
stf <7o5rfu92t@ctrlc.hu>
2019-03-07 03:09:05 +0100

    [mod] removed redundant change0 test case

--- tests/change.py
+++ tests/change.py
@@ -25,16 +25,46 @@ def test(a, b):
         for x in sorted(tmp,key=lambda x: len(x['path'])): print(x)
     assert (tmp==[])
 
-#diff [{'type': 'changed', 'path': ['b', 0], 'data': ([], {'2': []})}, {'type': 'changed', 'path': ['b', 1], 'data': ({'2': []}, [])}, {'type': 'added', 'path': ['b', 2, 0], 'data': [[[1749]]]}, {'type': 'added', 'path': ['b', 2, 1], 'data': [17, 1749, 'ZJQ', 23]}, {'type': 'added', 'path': ['b', 2, 2], 'data': 1749}, {'type': 'added', 'path': ['b', 2, 3], 'data': {'2': 42, 'a': 42, '5': {}, '23': {'23': 5, '2': {}, 'a': []}}}, {'type': 'added', 'path': ['b', 2, 4], 'data': 'kXd'}, {'type': 'deleted', 'path': ['b', 3, 0], 'data': [[[1749]]]}, {'type': 'added', 'path': ['b', 3, 1], 'data': [[[1749]]]}, {'type': 'changed', 'path': ['b', 3, 1], 'data': ([17, 1749, 'ZJQ', 23], '7oL')}, {'type': 'deleted', 'path': ['b', 3, 1], 'data': [17, 1749, 'ZJQ', 23]}, {'type': 'added', 'path': ['b', 3, 2], 'data': [17, 1749, 'ZJQ', 23]}, {'type': 'changed', 'path': ['b', 3, 2], 'data': (1749, '81m')}, {'type': 'added', 'path': ['b', 3, 0], 'data': [[]]}, {'type': 'added', 'path': ['b', 3, 3], 'data': 'RSV'}, {'type': 'added', 'path': ['b', 3, 4], 'data': 42}, {'type': 'added', 'path': ['b', 3, 5], 'data': []}, {'type': 'deleted', 'path': ['b', 3, 2], 'data': 1749}, {'type': 'deleted', 'path': ['b', 3, 3], 'data': {'2': 42, 'a': 42, '5': {}, '23': {'23': 5, '2': {}, 'a': []}}}, {'type': 'deleted', 'path': ['b', 3, 4], 'data': 'kXd'}, {'type': 'deleted', 'path': ['b', 4], 'data': [[[]], '7oL', '81m', 'RSV', 42, []]}]
-
-#wtf change {'type': 'changed', 'path': ['b', 3, 1], 'data': ([17, 1749, 'ZJQ', 23], '7oL')} [[[]], [[[1749]]], [17, 1749, 'ZJQ', 23], 'RSV', 42, []]
-#wtf change {'type': 'changed', 'path': ['b', 3, 2], 'data': (1749, '81m')} [[[]], [[[1749]]], [17, 1749, 'ZJQ', 23], 'RSV', 42, []]
-#
-#Traceback (most recent call last):
-#  File "./fuzz.py", line 99, in <module>
-#    assert diff(new, patch(old, d)) in [None, []]
-#AssertionError
-
 old={'b': [[], {'2': []}, [], [[[[1749]]], [17, 1749, 'ZJQ', 23], 1749, {'2': 42, 'a': 42, '5': {}, '23': {'23': 5, '2': {}, 'a': []}}, 'kXd'], [[[]], '7oL', '81m', 'RSV', 42, []]], '1': {}, 'c': 3}
 new={'b': [{'2': []}, [], [[[[1749]]], [17, 1749, 'ZJQ', 23], 1749, {'2': 42, 'a': 42, '5': {}, '23': {'23': 5, '2': {}, 'a': []}}, 'kXd'], [[[]], '7oL', '81m', 'RSV', 42, []]], '1': {}, 'c': 3}
 test(old,new)
+
+#________________________________________________________________________________________________________________________
+#
+#{'type': 'changed', 'path': ['b', 0], 'data': ([], {'2': []})}
+#{'type': 'changed', 'path': ['b', 1], 'data': ({'2': []}, [])}
+#{'type': 'deleted', 'path': ['b', 4], 'data': [[[]], '7oL', '81m', 'RSV', 42, []]}
+#{'type': 'added', 'path': ['b', 2, 0], 'data': [[[1749]]]}
+#{'type': 'added', 'path': ['b', 2, 1], 'data': [17, 1749, 'ZJQ', 23]}
+#{'type': 'added', 'path': ['b', 2, 2], 'data': 1749}
+#{'type': 'added', 'path': ['b', 2, 3], 'data': {'2': 42, 'a': 42, '5': {}, '23': {'23': 5, '2': {}, 'a': []}}}
+#{'type': 'added', 'path': ['b', 2, 4], 'data': 'kXd'}
+#{'type': 'deleted', 'path': ['b', 3, 0], 'data': [[[1749]]]}
+#{'type': 'added', 'path': ['b', 3, 1], 'data': [[[1749]]]}
+#{'type': 'changed', 'path': ['b', 3, 1], 'data': ([17, 1749, 'ZJQ', 23], '7oL')}
+#{'type': 'deleted', 'path': ['b', 3, 1], 'data': [17, 1749, 'ZJQ', 23]}
+#{'type': 'added', 'path': ['b', 3, 2], 'data': [17, 1749, 'ZJQ', 23]}
+#{'type': 'changed', 'path': ['b', 3, 2], 'data': (1749, '81m')}
+#{'type': 'added', 'path': ['b', 3, 0], 'data': [[]]}
+#{'type': 'added', 'path': ['b', 3, 3], 'data': 'RSV'}
+#{'type': 'added', 'path': ['b', 3, 4], 'data': 42}
+#{'type': 'added', 'path': ['b', 3, 5], 'data': []}
+#{'type': 'deleted', 'path': ['b', 3, 2], 'data': 1749}
+#{'type': 'deleted', 'path': ['b', 3, 3], 'data': {'2': 42, 'a': 42, '5': {}, '23': {'23': 5, '2': {}, 'a': []}}}
+#{'type': 'deleted', 'path': ['b', 3, 4], 'data': 'kXd'}
+#
+#wtf change {'type': 'changed', 'path': ['b', 3, 1], 'data': ([17, 1749, 'ZJQ', 23], '7oL')} [[[]], [[[1749]]], [17, 1749, 'ZJQ', 23], 'RSV', 42, []]
+#wtf change {'type': 'changed', 'path': ['b', 3, 2], 'data': (1749, '81m')} [[[]], [[[1749]]], [17, 1749, 'ZJQ', 23], 'RSV', 42, []]
+#________________________________________________________________________________________________________________________
+#patch(a, diff(a,b))!=b
+#b2 {'b': [{'2': []}, [], [[[[1749]]], [17, 1749, 'ZJQ', 23], 1749, {'2': 42, 'a': 42, '5': {}, '23': {'23': 5, '2': {}, 'a': []}}, 'kXd'], [[[]], [[[1749]]], [17, 1749, 'ZJQ', 23], 'RSV', 42, []
+#]], '1': {}, 'c': 3}
+#diff
+#{'type': 'changed', 'path': ['b', 3, 1], 'data': ([[[1749]]], '7oL')}
+#{'type': 'changed', 'path': ['b', 3, 2], 'data': ([17, 1749, 'ZJQ', 23], '81m')}
+#Traceback (most recent call last):
+#  File "tests/change.py", line 40, in <module>
+#    test(old,new)
+#  File "tests/change.py", line 26, in test
+#    assert (tmp==[])
+#AssertionError
